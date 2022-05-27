f748ce82de0507ea80def31a31775f9f79e32936
sergimonserrat <86484321+sergimonserrat@users.noreply.github.com>
2022-05-27 20:02:44 +0200

    Update Monserrat_Mascaro_Sergi_getChange.py

--- Monserrat_Mascaro_Sergi_getChange.py
+++ Monserrat_Mascaro_Sergi_getChange.py
@@ -5,7 +5,22 @@ Created on Thu May 12 14:26:40 2022
 @author: Sergi
 """
 
+'''
+This code was presented as part of a hiring process. The exercise consisted in computing the change of a purchase and returning it in the optimal way
+given a series of available coin denominations.
+'''
+
 def getChange(amount, price):
+    '''
+    This function computes the coins needed to give back the change optimally.
+    
+    Input
+    amount: float. amount of money received
+    price: float. price of the purchased item
+    
+    Output
+    coin_hist: list. amount of coins of each denomination needed to give back the change.
+    '''
     change = amount-price
     coins = [0.01, 0.05, 0.10, 0.20, 0.50, 1.00] #Available coin values
     coin_hist = [0 for i in range(0,6)] #Histogram of needed coins
@@ -28,11 +43,6 @@ def getChange(amount, price):
 print(getChange(5, 0.99))
 print(getChange(3.14, 1.99))
 print(getChange(4, 3.14)) 
-'''
-There is a mistake on the formulation of the problem. The proposed solution
-[1, 0, 1, 1, 1, 0] = 0.81 dollars of change while 4-3.14 = 0.86 dollars. The
-solution [1, 1, 1, 1, 1, 0] presented here is correct.
-'''
 '''
 Coding time: aprox. 30 min
 Difficulty: easy
